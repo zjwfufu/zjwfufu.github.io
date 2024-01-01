4ad6fbd7ec151a68002a91ac84614305ff725333
zjwXDU <1174987765@qq.com>
2024-01-01 21:33:25 +0800

    Site updated: 2024-01-01 21:33:22

--- 3dgs/index.js
+++ 3dgs/index.js
@@ -19,15 +19,34 @@ canvas.addEventListener("mouseleave", () => {
     isInSpecialArea = false;
 });
 
+// 显示模态框
+function showModal() {
+    const modal = document.getElementById("progress-dialog");
+    modal.style.display = "block";
+}
+
+// 关闭模态框
+function closeModal() {
+    const modal = document.getElementById("progress-dialog");
+    modal.style.display = "none";
+}
+
 async function main() {
     //const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k.splat";
 	//const url = "/3dgs/bonsai-7k.splat";
-	const url = "https://huggingface.co/datasets/zjwfufu/gsplat_view/resolve/main/fufu.ply";
+	//const url = "https://huggingface.co/datasets/zjwfufu/gsplat_view/resolve/main/fufu.splat";
 	//const url = "https://huggingface.co/datasets/zjwfufu/gsplat_view/resolve/main/point_cloud.ply"
 	
-    //await SPLAT.Loader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
-	await SPLAT.PLYLoader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
-    progressDialog.close();
+	const modelDropdown = document.getElementById("modelDropdown");
+
+    // Set the initial model URL
+    const url = modelDropdown.querySelector('.dropdown-item').getAttribute('data-model-url');	
+	showModal();
+    await SPLAT.Loader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
+	//await SPLAT.PLYLoader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
+    // progressDialog.close();
+	closeModal();
+	progressIndicator.value = 0;
 
     const handleResize = () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -44,8 +63,34 @@ async function main() {
 
     handleResize();
     window.addEventListener("resize", handleResize);
+	
+	// Update scene when model is changed
+modelDropdown.addEventListener("click", async (event) => {
+    if (event.target.tagName === 'A') {
+		scene.reset();
+        //progressDialog.classList.add('centered-modal'); // 添加类
+        // progressDialog.showModal();
+		showModal();
+
+        // Clear the scene before loading the new model
+        
+        const newSelectedModelUrl = event.target.getAttribute('data-model-url');
+        
+        await loadAndRenderModel(newSelectedModelUrl);
+
+        // progressDialog.close();
+		closeModal();
+		progressIndicator.value = 0;
+        //progressDialog.classList.remove('centered-modal'); // 移除类
+    }
+});
 
     requestAnimationFrame(frame);
+}
+
+async function loadAndRenderModel(modelUrl) {
+    await SPLAT.Loader.LoadAsync(modelUrl, scene, (progress) => progressIndicator.value = progress * 100);
+    // You can perform additional rendering or setup logic here if needed
 }
 
 main();
